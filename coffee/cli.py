921eb872a9d5f670af1f085048760f81b906d760
oftl <oftl@rockandroll.at>
2018-01-23 22:44:37 +0100

    make cli.start() dispatch to handlers

--- coffee/cli.py
+++ coffee/cli.py
@@ -17,12 +17,67 @@ class CLI ():
 
     def __init__ (self):
         self.fsm = None
-        self.issues = None
+        self.issues = []
         self.loaded_from = []
         self.history = FileHistory (os.path.expanduser ('~/.coffee.history'))
         self.lives = 5
 
+        self.commands = [
+            dict (command = 'load',            description = 'load the given yaml file', handler = self.load,      usage = 'load <filename>'),
+            dict (command = '!',               description = 'run <command> in a shell', handler = self.shell,     usage = '! <command>'),
+            dict (command = 'print',           description = 'print loaded data',        handler = self.printer),
+            dict (command = 'print fsm',       description = 'print the FSM only',       handler = self.printer),
+            dict (command = 'print fsm-svg',   description = 'draw the FSM (SVG)',       handler = self.printer),
+            dict (command = 'print fsm-ascii', description = 'draw the FSM (ASCII)',     handler = self.printer),
+            dict (command = 'clear',           description = 'clear data',               handler = self.clear),
+            dict (command = 'quit',            description = 'quit',                     handler = self.quit),
+            dict (command = 'help',            description = 'you\'re reading it, HTH',  handler = self.usage),
+        ]
+
         self.start()
+
+
+    def start (self):
+        self.usage ()
+
+        # note: must put '-' as last to prevent its meaning for ranges
+        prog = re.compile ('^(?P<command>[\w!]+)( (?P<param>[\w./-]+)){0,1}')
+
+        while self.lives:
+            cmd = prompt (
+                '>> ',
+                completer = WordCompleter ('load print clear quit help fsm fsm-ascii fsm fsm-svg'.split()),
+                history = self.history,
+                auto_suggest = AutoSuggestFromHistory(),
+                get_bottom_toolbar_tokens = self.get_bottom_toolbar_tokens,
+                style = style_from_dict ({ Token.Toolbar: '#ffffff bg:#333333' }),
+            )
+
+            result = (prog.match (cmd))
+
+            #  print ('cmd: {}'.format(cmd))
+            #  print ('command: {}'.format(result.group('command')))
+            #  print ('param: {}'.format(result.group('param')))
+
+            command = [c for c in self.commands if c.get ('command') == result.group ('command')]
+
+            if not len (command):
+                print ('ERROR: invalid command, try again\n')
+                continue
+
+            try:
+                if result.group('param'):
+                    command.pop().get('handler')(result.group('param'))
+                else:
+                    command.pop().get('handler')()
+
+            except Exception as e:
+                print ('ERROR: invalid command, try again: {}\n'.format (e))
+                raise e
+                self.lives -= 1
+                self.usage()
+
+        print ('\n\n\tG A M E   O V E R\n\n')
 
 
     def get_bottom_toolbar_tokens (self, cli):
@@ -37,88 +92,137 @@ class CLI ():
         ))]
 
 
-    def print_all (self, **kwa):
-        fsm = kwa.get ('fsm')
-        issues = kwa.get ('issues')
+    def printer (self, *kw):
+        # note: invalid commands such as 'print stupsi' will
+        # simply ignore the second word and do a 'print'
+        if len (kw) == 1:
+            arg = kw[0]
+        else:
+            arg = None
 
-        print ()
-        for state in fsm.workflow():
-            print ('  (*) {}'.format (state.name))
-            for issue in list (filter (lambda i: i.state == state.name, issues)):
-                print ('    (-) {}'.format (issue.title))
-        print ()
+        if not self.fsm:
+            print ('ERROR: Please load FSM\n')
+            self.lives -= 1
+            return
 
+        if arg == 'fsm-ascii':
+            try:
+                graph (
+                    fsm          = self.fsm,
+                    source_files = sorted (self.loaded_from),
+                    fmt          = 'ascii',
+                )
+            except Exception as e:
+                print ('ERROR: {}'.format (e))
 
-    def print_fsm (self, **kwa):
-        fsm = kwa.get ('fsm')
+        elif arg == 'fsm-svg':
+            try:
+                graph (
+                    fsm          = self.fsm,
+                    source_files = sorted (self.loaded_from),
+                    fmt          = 'svg',
+                )
+            except Exception as e:
+                print ('ERROR: {}'.format (e))
 
-        print ()
-        for state in fsm.workflow():
-            print ('  (*) {name} ({next_states}){is_start}{is_end}'.format (
-                name = state.name,
-                next_states = ', '.join (
-                    '-> {}'.format (e.next_state.name)
-                    for e in sorted (state.events, key = lambda e: e.next_state.name)) or '-',
-                is_start = ' (S)' if state.is_start else '',
-                is_end = ' (E)' if state.is_end else '',
-            ))
+        elif arg == 'fsm':
+            print ()
+            for state in self.fsm.workflow():
+                print ('  (*) {name} ({next_states}){is_start}{is_end}'.format (
+                    name = state.name,
+                    next_states = ', '.join (
+                        '-> {}'.format (e.next_state.name)
+                        for e in sorted (state.events, key = lambda e: e.next_state.name)) or '-',
+                    is_start = ' (S)' if state.is_start else '',
+                    is_end = ' (E)' if state.is_end else '',
+                ))
 
-        print ()
-        print ('(S) ... state is a start state')
-        print ('(E) ... state is an end state')
-        print ()
+            print ()
+            print ('(S) ... state is a start state')
+            print ('(E) ... state is an end state')
+            print ()
 
-
-    def draw_fsm (self, **kwa):
-        try:
-            graph (
-                fsm          = self.fsm,
-                source_files = sorted (self.loaded_from),
-                output_file  = kwa.get ('output_file'),
-                fmt          = kwa.get ('fmt'),
-            )
-        except Exception as e:
-            print ('ERROR: {}'.format (e))
-
-
-    def load (self, **kwa):
-        filename = kwa.get ('filename')
-
-        with open (filename, 'r') as ymlfile:
-            fsm, issues = None, None
-            inputs = yaml.load (ymlfile)
-
-            if type (inputs) != dict:
-                print ('ERROR: can\'t load from <{}>\n'.format (filename))
+        else:
+            if not self.issues:
+                print ('ERROR: Please load issues\n')
                 self.lives -= 1
-                return dict (fsm = None, issues = None)
+                return
 
-            if inputs.get ('issues'):
-                issues = [
-                    Issue (
-                        title = issue.get ('title'),
-                        state = issue.get ('state'),
-                    )
-                    for issue in inputs.get ('issues')
-                ]
+            print ()
+            for state in self.fsm.workflow():
+                print ('  (*) {name} {is_start}{is_end}'.format (
+                    name = state.name,
+                    is_start = ' (S)' if state.is_start else '',
+                    is_end = ' (E)' if state.is_end else '',
+                ))
+                for issue in list (filter (lambda i: i.state == state.name, self.issues)):
+                    print ('    (-) {}'.format (issue.title))
 
-            if inputs.get ('transitions'):
-                fsm = FSM (transitions = inputs.get ('transitions'))
+            print ()
+            print ('(S) ... state is a start state')
+            print ('(E) ... state is an end state')
+            print ()
 
-            return dict (fsm = fsm, issues = issues)
+
+    def load (self, filename):
+        if not filename or not os.path.isfile (filename):
+            print ('ERROR: Please enter a valid filename\n')
+            self.lives -= 1
+            return
+
+        try:
+            loaded_ok = False
+
+            with open (filename, 'r') as ymlfile:
+                fsm, issues = None, None
+                inputs = yaml.load (ymlfile)
+
+                if type (inputs) != dict:
+                    raise RuntimeWarning ('can\'t load data')
+
+                if inputs.get ('issues'):
+                    self.issues = [
+                        Issue (
+                            title = issue.get ('title'),
+                            state = issue.get ('state'),
+                        )
+                        for issue in inputs.get ('issues')
+                    ]
+                    loaded_ok = True
+
+                if inputs.get ('transitions'):
+                    self.fsm = FSM (transitions = inputs.get ('transitions'))
+                    loaded_ok = True
+
+                if loaded_ok:
+                    self.loaded_from.append (filename)
+                else:
+                    raise RuntimeWarning ('can\'t load data')
+
+        except Exception as w:
+            print ('ERROR: invalid data: <{}>\n'.format (w))
+            self.lives -= 1
 
 
     def usage (self):
-        print ('\nAvailable commands')
-        print ('  load <filename> ..... load the given yaml file')
-        print ('  !<command> .......... run <command> in a shell')
-        print ('  print ............... print loaded data')
-        print ('  print fsm ........... print the FSM only')
-        print ('  print fsm-svg ....... draw the FSM (SVG)')
-        print ('  print fsm-ascii ..... draw the FSM (ASCII)')
-        print ('  clear ............... clear data')
-        print ('  quit ................ quit')
-        print ('  help ................ you\'re reading it, HTH\n')
+        # find the longest line for padding of right column
+        max_len = (
+            8
+          + max ([len (c.get ('usage') or c.get ('command')) for c in self.commands])
+          + max ([len (c.get ('description')) for c in self.commands])
+        )
+
+        usage = 'Available commands\n'
+        usage += '\n'.join ([
+            '  {command} {description:.>{padding}}'.format (
+                padding     = max_len - len (c.get ('usage') or c.get ('command')),
+                command     = c.get ('usage') or c.get ('command'),
+                description = ' {}'.format (c.get ('description')),
+            )
+            for c in self.commands
+        ])
+
+        print ('\n{}\n'.format (usage))
 
 
     def clear (self):
@@ -127,92 +231,20 @@ class CLI ():
         self.loaded_from = []
 
 
-    def start (self):
-        self.usage()
+    def shell (self, cmd):
+        print (subprocess.run (
+            cmd.split(),
+            stdout = subprocess.PIPE,
+        ).stdout.decode())
 
-        while self.lives:
-            cmd = prompt (
-                '>> ',
-                completer = WordCompleter ('load print clear quit help'.split()),
-                history = self.history,
-                auto_suggest = AutoSuggestFromHistory(),
-                get_bottom_toolbar_tokens = self.get_bottom_toolbar_tokens,
-                style = style_from_dict ({ Token.Toolbar: '#ffffff bg:#333333' }),
-            )
 
-            if re.search ('^load (.+)', cmd):
-                filename = cmd [5:]
+    def quit (self):
+        exit()
 
-                if not filename or not os.path.isfile (filename):
-                    print ('ERROR: Please enter a valid filename\n')
-                    self.lives -= 1
-                    continue
 
-                try:
-                    jim = self.load (filename = filename)
-                except RuntimeWarning as w:
-                    print ('ERROR: invalid data: <{}>\n'.format (w))
-                    self.lives -= 1
-                    continue
+    def oneup (self):
+        self.lives += 1
 
-                self.loaded_from.append (filename)
-                loaded_ok = False
-
-                if jim.get ('fsm'):
-                    self.fsm = jim.get ('fsm')
-                    loaded_ok = True
-                if jim.get ('issues'):
-                    self.issues = jim.get ('issues')
-                    loaded_ok = True
-                if not loaded_ok:
-                    len (self.loaded_from) and self.loaded_from.pop()
-            elif re.search ('^!(.+)', cmd):
-                print (subprocess.run (
-                    cmd[1:].split(),
-                    stdout = subprocess.PIPE,
-                ).stdout.decode())
-            elif re.search ('^print.*', cmd):
-                if not self.fsm:
-                    print ('ERROR: Please load an FSM\n')
-                    self.lives -= 1
-                    continue
-                if cmd == 'print':
-                    if not self.issues:
-                        print ('ERROR: Please load issues\n')
-                        self.lives -= 1
-                        continue
-                    self.print_all (fsm = self.fsm, issues = self.issues)
-                else:
-                    if cmd == 'print fsm':
-                        self.print_fsm (fsm = self.fsm)
-                    elif cmd == 'print fsm-svg':
-                        self.draw_fsm (
-                            fsm = self.fsm,
-                            fmt = 'svg',
-                        )
-                    elif cmd == 'print fsm-ascii':
-                        self.draw_fsm (
-                            fsm = self.fsm,
-                            fmt = 'ascii',
-                        )
-                    else:
-                        print ('ERROR: invalid command, try again :-)\n')
-                        self.lives -= 1
-                        self.usage()
-            elif cmd == 'clear':
-                self.clear()
-            elif cmd == 'quit':
-                exit()
-            elif cmd == 'help':
-                self.usage()
-            elif cmd == 'oneup':
-                self.lives += 1
-            else:
-                print ('ERROR: invalid command, try again :-)\n')
-                self.lives -= 1
-                self.usage()
-
-        print ('\n\n\tG A M E   O V E R\n\n')
 
 if __name__ == '__main__':
     CLI().start()
